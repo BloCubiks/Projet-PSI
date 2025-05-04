021e12d52f3601de6b5677425492d50e64282a8e
Overzam <thomidu95@gmail.com>
2025-05-04 15:16:26 +0200

    xml & json import/export
    pitié dites moi que ça marche

---
+++ PSI Livrable 1/JSON.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PSI_Livrable_1
+{
+    class JSON
+    {
+        static string connectionString = "server=localhost;user=root;password=root;database=psi;";
+
+        static void Main(string[] args)
+        {
+            InsertCuisiniers("Cuisiniers.json");
+            InsertParticuliers("Particuliers.json");
+            InsertEntreprises("Entreprises.json");
+            InsertCommandes("Commandes.json");
+            InsertPlats("Plats.json");
+            InsertIngredients("Ingredients.json");
+            InsertEstCompose("EstCompose.json");
+            InsertContient("Contient.json");
+        }
+
+        static void InsertCuisiniers(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var c in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Cuisinier VALUES (@id, @nom, @prenom, @adresse, @cp, @ville, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)c.NumeroCuisinier);
+                    cmd.Parameters.AddWithValue("@nom", (string)c.NomC);
+                    cmd.Parameters.AddWithValue("@prenom", (string)c.PrenomC);
+                    cmd.Parameters.AddWithValue("@adresse", (string)c.AdresseC);
+                    cmd.Parameters.AddWithValue("@cp", (string)c.CodePostalC);
+                    cmd.Parameters.AddWithValue("@ville", (string)c.VilleC);
+                    cmd.Parameters.AddWithValue("@tel", (string)c.TelC);
+                    cmd.Parameters.AddWithValue("@email", (string)c.EmailC);
+                    cmd.Parameters.AddWithValue("@metro", (string)c.MetroC);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertParticuliers(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var p in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Particulier VALUES (@id, @nom, @prenom, @adresse, @cp, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)p.NumeroParticulier);
+                    cmd.Parameters.AddWithValue("@nom", (string)p.NomP);
+                    cmd.Parameters.AddWithValue("@prenom", (string)p.PrenomP);
+                    cmd.Parameters.AddWithValue("@adresse", (string)p.AdresseP);
+                    cmd.Parameters.AddWithValue("@cp", (string)p.CodePostalP);
+                    cmd.Parameters.AddWithValue("@tel", (string)p.TelP);
+                    cmd.Parameters.AddWithValue("@email", (string)p.EmailP);
+                    cmd.Parameters.AddWithValue("@metro", (string)p.MetroP);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertEntreprises(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var e in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Entreprise VALUES (@id, @nom, @contact, @adresse, @cp, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)e.NumeroEntreprise);
+                    cmd.Parameters.AddWithValue("@nom", (string)e.NomE);
+                    cmd.Parameters.AddWithValue("@contact", (string)e.ContactE);
+                    cmd.Parameters.AddWithValue("@adresse", (string)e.AdresseE);
+                    cmd.Parameters.AddWithValue("@cp", (string)e.CodePostalE);
+                    cmd.Parameters.AddWithValue("@tel", (string)e.TelE);
+                    cmd.Parameters.AddWithValue("@email", (string)e.EmailE);
+                    cmd.Parameters.AddWithValue("@metro", (string)e.MetroE);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertCommandes(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var c in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Commande (IDCommande, DateCommande, AdresseLivraison, Satisfaction, NumeroCuisinier, NumeroParticulier, NumeroEntreprise) VALUES (@id, @date, @adresse, @satisfaction, @cuisinier, @particulier, @entreprise)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)c.IDCommande);
+                    cmd.Parameters.AddWithValue("@date", DateTime.Parse((string)c.DateCommande));
+                    cmd.Parameters.AddWithValue("@adresse", (string)c.AdresseLivraison);
+                    cmd.Parameters.AddWithValue("@satisfaction", (int)c.Satisfaction);
+                    cmd.Parameters.AddWithValue("@cuisinier", c.NumeroCuisinier != null ? (int)c.NumeroCuisinier : (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@particulier", c.NumeroParticulier != null ? (int)c.NumeroParticulier : (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@entreprise", c.NumeroEntreprise != null ? (int)c.NumeroEntreprise : (object)DBNull.Value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertPlats(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var p in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Plat VALUES (@id, @nom, @prix, @quantite, @type, @dateFab, @datePer, @regime, @nat, @cuisinier)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)p.IdPlat);
+                    cmd.Parameters.AddWithValue("@nom", (string)p.NomPlat);
+                    cmd.Parameters.AddWithValue("@prix", (decimal)p.Prix);
+                    cmd.Parameters.AddWithValue("@quantite", (int)p.Quantite);
+                    cmd.Parameters.AddWithValue("@type", (string)p.TypePlat);
+                    cmd.Parameters.AddWithValue("@dateFab", DateTime.Parse((string)p.DateFabrication));
+                    cmd.Parameters.AddWithValue("@datePer", DateTime.Parse((string)p.DatePeremption));
+                    cmd.Parameters.AddWithValue("@regime", (string)p.RegimeAlim);
+                    cmd.Parameters.AddWithValue("@nat", (string)p.Nationalite);
+                    cmd.Parameters.AddWithValue("@cuisinier", (int)p.NumeroCuisinier);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertIngredients(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var i in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Ingredient (IdIngredient, Nom, Quantite) VALUES (@id, @nom, @quantite)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)i.IdIngredient);
+                    cmd.Parameters.AddWithValue("@nom", (string)i.Nom);
+                    cmd.Parameters.AddWithValue("@quantite", (int)i.Quantite);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertEstCompose(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var ec in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO EstCompose VALUES (@plat, @ingredient)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@plat", (int)ec.IdPlat);
+                    cmd.Parameters.AddWithValue("@ingredient", (int)ec.IdIngredient);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertContient(string filePath)
+        {
+            var data = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(filePath));
+            foreach (var c in data)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Contient VALUES (@commande, @plat)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@commande", (int)c.IdCommande);
+                    cmd.Parameters.AddWithValue("@plat", (int)c.IdPlat);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+    class ExportJson
+    {
+        static string connectionString = "Server=localhost;Database=psi;Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            ExportTableToJson("Cuisinier");
+            ExportTableToJson("Particulier");
+            ExportTableToJson("Entreprise");
+            ExportTableToJson("Commande");
+            ExportTableToJson("Plat");
+            ExportTableToJson("Ingredient");
+            ExportTableToJson("EstCompose");
+            ExportTableToJson("Contient");
+
+            Console.WriteLine("Export JSON terminé.");
+        }
+
+        static void ExportTableToJson(string tableName)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = $"SELECT * FROM {tableName}";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                string json = JsonConvert.SerializeObject(dt, Formatting.Indented);
+                File.WriteAllText($"{tableName}.json", json);
+
+                Console.WriteLine($"{tableName}.json exporté.");
+            }
+        }
+    }
+}
---
+++ PSI Livrable 1/XML.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace PSI_Livrable_1
+{
+    class XML
+    {
+        static string connectionString = "server=localhost;user=root;password=root;database=psi;";
+
+        static void Main(string[] args)
+        {
+            InsertCuisiniers("Cuisiniers.xml");
+            InsertParticuliers("Particuliers.xml");
+            InsertEntreprises("Entreprises.xml");
+            InsertCommandes("Commandes.xml");
+            InsertPlats("Plats.xml");
+            InsertIngredients("Ingredients.xml");
+            InsertEstCompose("EstCompose.xml");
+            InsertContient("Contient.xml");
+        }
+
+        static void InsertCuisiniers(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Cuisinier"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Cuisinier VALUES (@id, @nom, @prenom, @adresse, @cp, @ville, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("NumeroCuisinier"));
+                    cmd.Parameters.AddWithValue("@nom", (string)element.Element("NomC"));
+                    cmd.Parameters.AddWithValue("@prenom", (string)element.Element("PrenomC"));
+                    cmd.Parameters.AddWithValue("@adresse", (string)element.Element("AdresseC"));
+                    cmd.Parameters.AddWithValue("@cp", (string)element.Element("CodePostalC"));
+                    cmd.Parameters.AddWithValue("@ville", (string)element.Element("VilleC"));
+                    cmd.Parameters.AddWithValue("@tel", (string)element.Element("TelC"));
+                    cmd.Parameters.AddWithValue("@email", (string)element.Element("EmailC"));
+                    cmd.Parameters.AddWithValue("@metro", (string)element.Element("MetroC"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertParticuliers(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Particulier"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Particulier VALUES (@id, @nom, @prenom, @adresse, @cp, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("NumeroParticulier"));
+                    cmd.Parameters.AddWithValue("@nom", (string)element.Element("NomP"));
+                    cmd.Parameters.AddWithValue("@prenom", (string)element.Element("PrenomP"));
+                    cmd.Parameters.AddWithValue("@adresse", (string)element.Element("AdresseP"));
+                    cmd.Parameters.AddWithValue("@cp", (string)element.Element("CodePostalP"));
+                    cmd.Parameters.AddWithValue("@tel", (string)element.Element("TelP"));
+                    cmd.Parameters.AddWithValue("@email", (string)element.Element("EmailP"));
+                    cmd.Parameters.AddWithValue("@metro", (string)element.Element("MetroP"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertEntreprises(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Entreprise"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Entreprise VALUES (@id, @nom, @contact, @adresse, @cp, @tel, @email, @metro)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("NumeroEntreprise"));
+                    cmd.Parameters.AddWithValue("@nom", (string)element.Element("NomE"));
+                    cmd.Parameters.AddWithValue("@contact", (string)element.Element("ContactE"));
+                    cmd.Parameters.AddWithValue("@adresse", (string)element.Element("AdresseE"));
+                    cmd.Parameters.AddWithValue("@cp", (string)element.Element("CodePostalE"));
+                    cmd.Parameters.AddWithValue("@tel", (string)element.Element("TelE"));
+                    cmd.Parameters.AddWithValue("@email", (string)element.Element("EmailE"));
+                    cmd.Parameters.AddWithValue("@metro", (string)element.Element("MetroE"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertCommandes(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Commande"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Commande (IDCommande, DateCommande, AdresseLivraison, Satisfaction, NumeroCuisinier, NumeroParticulier, NumeroEntreprise) VALUES (@id, @date, @adresse, @satisfaction, @cuisinier, @particulier, @entreprise)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("IDCommande"));
+                    cmd.Parameters.AddWithValue("@date", DateTime.Parse((string)element.Element("DateCommande")));
+                    cmd.Parameters.AddWithValue("@adresse", (string)element.Element("AdresseLivraison"));
+                    cmd.Parameters.AddWithValue("@satisfaction", (int)element.Element("Satisfaction"));
+                    cmd.Parameters.AddWithValue("@cuisinier", (int?)element.Element("NumeroCuisinier") ?? (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@particulier", (int?)element.Element("NumeroParticulier") ?? (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@entreprise", (int?)element.Element("NumeroEntreprise") ?? (object)DBNull.Value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertPlats(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Plat"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Plat VALUES (@id, @nom, @prix, @quantite, @type, @dateFab, @datePer, @regime, @nat, @cuisinier)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("IdPlat"));
+                    cmd.Parameters.AddWithValue("@nom", (string)element.Element("NomPlat"));
+                    cmd.Parameters.AddWithValue("@prix", decimal.Parse((string)element.Element("Prix")));
+                    cmd.Parameters.AddWithValue("@quantite", int.Parse((string)element.Element("Quantite")));
+                    cmd.Parameters.AddWithValue("@type", (string)element.Element("TypePlat"));
+                    cmd.Parameters.AddWithValue("@dateFab", DateTime.Parse((string)element.Element("DateFabrication")));
+                    cmd.Parameters.AddWithValue("@datePer", DateTime.Parse((string)element.Element("DatePeremption")));
+                    cmd.Parameters.AddWithValue("@regime", (string)element.Element("RegimeAlim"));
+                    cmd.Parameters.AddWithValue("@nat", (string)element.Element("Nationalite"));
+                    cmd.Parameters.AddWithValue("@cuisinier", (int)element.Element("NumeroCuisinier"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertIngredients(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Ingredient"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Ingredient (IdIngredient, Nom, Quantite) VALUES (@id, @nom, @quantite)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", (int)element.Element("IdIngredient"));
+                    cmd.Parameters.AddWithValue("@nom", (string)element.Element("Nom"));
+                    cmd.Parameters.AddWithValue("@quantite", (int)element.Element("Quantite"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertEstCompose(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("EstCompose"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO EstCompose VALUES (@plat, @ingredient)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@plat", (int)element.Element("IdPlat"));
+                    cmd.Parameters.AddWithValue("@ingredient", (int)element.Element("IdIngredient"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void InsertContient(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            foreach (XElement element in doc.Root.Elements("Contient"))
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Contient VALUES (@commande, @plat)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@commande", (int)element.Element("IdCommande"));
+                    cmd.Parameters.AddWithValue("@plat", (int)element.Element("IdPlat"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+    class ExportXML
+    {
+        static string connectionString = "Server=localhost;Database=psi;Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            ExportTableToXml("Cuisinier");
+            ExportTableToXml("Particulier");
+            ExportTableToXml("Entreprise");
+            ExportTableToXml("Commande");
+            ExportTableToXml("Plat");
+            ExportTableToXml("Ingredient");
+            ExportTableToXml("EstCompose");
+            ExportTableToXml("Contient");
+
+            Console.WriteLine("Export XML terminé.");
+        }
+
+        static void ExportTableToXml(string tableName)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = $"SELECT * FROM {tableName}";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable(tableName);
+                adapter.Fill(dt);
+
+                using (FileStream fs = new FileStream($"{tableName}.xml", FileMode.Create))
+                {
+                    dt.WriteXml(fs, XmlWriteMode.WriteSchema);
+                }
+
+                Console.WriteLine($"{tableName}.xml exporté.");
+            }
+        }
+    }
+}
